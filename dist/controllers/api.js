f9fec01c4e605114420c9a5cec00feba5c8f95a7
Thomas D Williams <dwilliams@halehan.com>
2018-01-22 15:00:54 -0600

    Added json and config files

---
+++ dist/controllers/api.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sample = (req, res) => {
+    res.json({ message: 'API Function Works' });
+};
+//# sourceMappingURL=api.js.map
