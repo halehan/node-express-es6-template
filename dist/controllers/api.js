1d1304b86a39c88b654b06f676898ec6dde90d9a
Thomas D Williams <dwilliams@halehan.com>
2018-01-22 15:18:16 -0600

    removed dist

--- dist/controllers/api.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sample = (req, res) => {
-    res.json({ message: 'API Function Works' });
-};
-//# sourceMappingURL=api.js.map
